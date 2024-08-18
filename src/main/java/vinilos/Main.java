fc6fbd3acf92f8df0ccd58a02a82d69823a09297
David <d.baez03@ufromail.cl>
2024-08-18 17:55:18 -0400

    Codigo terminado y totalmente funcional

--- src/main/java/vinilos/Main.java
+++ src/main/java/vinilos/Main.java
@@ -1,6 +1,7 @@
 package vinilos;
 
 import java.lang.invoke.StringConcatFactory;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,6 +9,15 @@ public class Main {
         agregarVinilo(vinilos, "Coldplay","A head full of dreams","2015");
         agregarVinilo(vinilos, "Iron Maiden","Killers","1981");
         printVinilos(vinilos);
+        String artista = "Coldplay";
+        String artista2= "Red hot chilli peppers";
+
+        mostrarBusquedaArtista(vinilos,artista2);
+        mostrarBusquedaArtista(vinilos,artista);
+
+        retornarDisponibles(vinilos);
+
+        retornarTotalVinilos(vinilos);
 
     }
     public static void agregarVinilo(String matriz[][], String group, String disc, String release) {
@@ -32,5 +42,54 @@ public class Main {
 
         }
 
+    }
+    public static Boolean buscarArtista (String matriz[][], String group ) {
+         Boolean validacion = false;
+        for (int i = 0; i < matriz.length; i++) {
+            if (Objects.equals(matriz[i][0], group)){
+                validacion = true;
+                break;
+            }
+            else {
+               validacion = false;
+               break;
+            }
+        }
+        return  (validacion);
+    }
+    public static void mostrarBusquedaArtista(String matriz [][], String group) {
+        if (buscarArtista(matriz, group) == true) {
+            System.out.print("El artista " + group + " si esta en la coleccion" + "\n");
+        }
+        else{
+            System.out.print("El artista " + group + " no esta en la coleccion" + "\n");
+        }
+    }
+    public static int disponibles(String matriz[][]){
+        int contadorVacios = 0;
+        for (int i = 0; i < matriz.length; i++) {
+            if (matriz[i][0] == null){
+                contadorVacios++;
+            }
+        }
+        return contadorVacios;
+    }
+    public static void retornarDisponibles(String matriz[][]){
+        System.out.print("Hay un total de: "+ disponibles(matriz)+ " espacios disponibles en la coleccion de vinilos\n" );
+    }
+    public static int totalVinilosEnColeccion(String matriz[][]){
+        int contadorVinilos = 0;
+        for (int i = 0; i < matriz.length; i++) {
+            if (matriz[i][0] == null){
+                break;
+            }
+            else {
+                contadorVinilos++;
+            }
+        }
+        return contadorVinilos;
+    }
+    public static void retornarTotalVinilos(String matriz[][]){
+        System.out.print("Hay " + totalVinilosEnColeccion(matriz) + " vinilos en la coleccion\n" );
     }
 }
